413f865f63d71c6eef4319d53f1586d4853cf76d
Alisson Souto <alissonprogp1@gmail.com>
2024-10-20 09:17:38 -0300

    Add Method analisarCandidato

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -2,6 +2,17 @@ package candidatura;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        
+
+    }
+    static void analisarCadidato(double salarioPretendido){
+        double salarioBase = 2000.0;
+
+        if (salarioBase > salarioPretendido) {
+            System.out.println("Ligar Para Candidato");
+        } else if(salarioBase == salarioPretendido){
+            System.out.println("Ligar Para O Candidato Com Contra Proposta");
+        } else {
+            System.out.println("Aguardando Os Demais Candidatos");
+        }
     }
 }
